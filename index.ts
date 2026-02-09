7433163ea8dd5ba75872d8560b70262b0c3229e2
Coussement Cédric (s157201) <cedric.coussement@student.ap.be>
2026-02-09 16:54:26 +0000

    url variabelen aangemaakt

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 app.set("port", 3000)
-
+let pokeBoxesurl = "https://raw.githubusercontent.com/preystation92/milestone_0_Dataset/refs/heads/main/jsonFiles/pokemonBoxes.json"
 let shopName: string = "PokeDeck Terminal"
 let shopDescription = "is een online platform gericht op de verkoop van Pokémonkaarten.Gebruikers kunnen kaarten bekijken, details raadplegen en veilig aankopen doen.De website is ontworpen voor zowel beginnende trainers als ervaren verzamelaars die op zoek zijn naar specifieke kaarten."
 
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
 
 
 app.get("/card", async (req, res) => {
-  let response = await fetch("https://raw.githubusercontent.com/preystation92/Milestone_1_Terminal-App/main/milestone-1/jsonFiles/pokemonCards.json")
+  let response = await fetch("https://raw.githubusercontent.com/preystation92/milestone_0_Dataset/refs/heads/main/jsonFiles/pokemonBoxes.json")
   let pokemonCards: PokemonCard = await response.json();
   res.render("card", { pokemonCards, shopName, shopDescription });
 });
